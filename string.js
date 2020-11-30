334c680ff295723a56f7ad7da1047feec0377471
Michael Sekol <michael.sekol@mahoningctc.com>
2020-11-30 11:01:42 -0500

    first commit

---
+++ string.js
@@ -0,0 +1,12 @@
+// alert("Samurai are awesome!")
+
+var x = 5;
+var y = 10;
+var xSquared = Math.pow(x, 2);
+var ySquared = Math.pow(y, 2);
+var z = Math.sqrt(xSquared +ySquared);
+var name = "Ninja";
+console.log("Your name is "+name);
+console.log("x + y = " + x+y);
+console.log("x + y = " + parseInt(x+y));
+console.log("x + y = " + "Ninja");
