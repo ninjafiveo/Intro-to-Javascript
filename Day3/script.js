de2bfd0ff587aaae78dce71f6da67e75ca9760ab
Michael Sekol <michael.sekol@mahoningctc.com>
2020-12-09 11:22:43 -0500

    Day 3 - Functions

---
+++ Day3/script.js
@@ -0,0 +1,37 @@
+/// Notes
+/// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+/// https://www.w3schools.com/jsref/dom_obj_document.asp
+
+
+document.getElementById("pageTitle").innerHTML = "Welcome to Ninjaing 101";
+// numbOne = document.getElementById("numberOne").value;
+
+function theAnswer(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    // alert(numbOne + numbTwo)
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne + numbTwo);
+};
+
+function mathSubtraction(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    // alert(numbOne + numbTwo)
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne - numbTwo);
+};
+
+function mathMultiply(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    // alert(numbOne + numbTwo)
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne * numbTwo);
+};
+
+function mathDivision(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    // alert(numbOne + numbTwo)
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne / numbTwo);
+};
+
+
