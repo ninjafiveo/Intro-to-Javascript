b00c4ee3a2b5ed1378802a80b59b2b93963ee7a9
ninjafiveo <ninjafiveo@gmail.com>
2021-01-13 11:26:57 -0500

    Updated to a walkthrough

--- Day3/Xscript.js
+++ Day3/Xscript.js
@@ -1,8 +1,15 @@
-document.getElementById("pageTitle").innerHTML = "Mr. Sekol Coding Calculator";
+/// Notes #12/9/2020
+/// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+/// https://www.w3schools.com/jsref/dom_obj_document.asp
+
+
+document.getElementById("pageTitle").innerHTML = "Welcome to Ninja Coding 101";
+// numbOne = document.getElementById("numberOne").value;
 
 function mathAddition(){
     let numbOne = parseFloat(document.getElementById("numberOne").value);
     let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    // alert(numbOne + numbTwo)
     document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne + numbTwo);
 };
 
@@ -28,5 +35,3 @@ function mathDivision(){
 };
 
 
-// alert(numbOne + numbTwo)
-// document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat(numbOne + numbTwo);
